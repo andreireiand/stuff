470f82b67ffdccd878d1018f8ecc5113ed71478f
andreireiand <32570141+andreireiand@users.noreply.github.com>
2020-03-17 14:26:19 +0000

    Add files via upload

---
+++ BootAdminIPs.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os, sys, fcntl, time, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--bootstrapFile", default='foo.txt')
+parser.add_argument("--mxIP1", default='1.2.3.4')
+parser.add_argument("--mxIP2", default='5.6.7.8')
+
+
+def RemoveAdminIPs(lines):
+    newLines = []
+    for line in lines:
+        if "admin-ip" not in line.strip().lower():
+            newLines.append(line)
+    return newLines
+
+def  AddAdminIPs(f, IP1, IP2):
+    f.write("<admin-ips>\n")
+    f.write(' <admin-ip ip=\"' + IP1 + '\"/>\n')
+    f.write(' <admin-ip ip=\"' + IP2 + '\"/>\n')        
+    f.write("</admin-ips>\n")        
+    
+
+try:
+    args = parser.parse_args()
+    
+    if args.bootstrapFile and args.mxIP1 and args.mxIP2:
+        print("fixing bootstrapfile ... ")
+        
+        f = open(args.bootstrapFile, "r+")
+        fcntl.flock(f.fileno(), fcntl.LOCK_EX)
+        
+        lines = f.readlines()
+        lines = RemoveAdminIPs(lines)
+        assert("</bootstrap>" in lines[-1].strip().lower()) # file must be properly formatted upon opening
+        
+        f.seek(0)
+        f.writelines(lines[:-1])
+        
+        AddAdminIPs(f, args.mxIP1, args.mxIP2)
+        
+        f.write(lines[-1])
+            
+        f.truncate()
+        fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+        f.close()
+        print("file changed successfully ... ")
+        
+    else:
+        raise ValueError("Not all args provided for Mx ActionSet and Action!")
+        
+except NameError:
+    f.write(lines[-1])
+    print("Unexpected NameError:", sys.exc_info()[0])
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    
+finally:
+    if not f.closed:
+        f.truncate()
+        fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+        f.close()        
+
